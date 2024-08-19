59a1f557d9f261ede5cbb8a63212e90b38fa84af
El Mehdi Faiz <mehdifaiz42@outlook.com>
2024-08-19 16:46:47 +0200

    working on sliding on walls

--- player_move.c
+++ player_move.c
@@ -1,4 +1,5 @@
 #include "cub3d.h"
+#include <stdio.h>
 
 int door_state(int x, int y, t_cube *cube)
 {
@@ -12,19 +13,36 @@ int door_state(int x, int y, t_cube *cube)
 
 int	check_player_position(float player_x, float player_y, t_cube *cube)
 {
+	int flag;
+
+	flag = 1;
 	if(cube->map[(int)((player_x + cube->radius))][(int)((player_y + cube->radius))] == '#' ||
 			(door_state((int)((player_x + cube->radius)), (int)((player_y + cube->radius)), cube)))
-		return (0);
+		flag = 0;
 	if(cube->map[(int)((player_x + cube->radius))][(int)((player_y - cube->radius))] == '#' ||
 			(door_state((int)((player_x + cube->radius)), (int)((player_y - cube->radius)), cube)))
-		return (0);
+		flag = 0;
 	if(cube->map[(int)((player_x - cube->radius))][(int)((player_y + cube->radius))] == '#' ||
 			(door_state((int)((player_x - cube->radius)), (int)((player_y + cube->radius)), cube)))
-		return (0);
+		flag = 0;
 	if(cube->map[(int)((player_x - cube->radius))][(int)((player_y - cube->radius))] == '#' ||
 			(door_state((int)((player_x - cube->radius)), (int)((player_y - cube->radius)), cube)))
-		return (0);
-	return (1);
+		flag = 0;
+	return (flag);
+}
+
+int player_dir(int dx, int dy)
+{
+	if (dx == 0 && dy == -1)
+		return(0);
+    if (dx == 0 && dy == 1)
+    	return(0);
+    if (dx == -1 && dy == 0)
+    	return (0);
+    if (dx == 1 && dy == 0)
+    	return (0);
+    else
+    	return (1);
 }
 
 void	move_player(t_cube *cube)
@@ -55,7 +73,16 @@ void	move_player(t_cube *cube)
 		move_x -= cube->player_dy/MINIMAP_SCALE;
 		move_y += cube->player_dx/MINIMAP_SCALE;
 	}
-	if(check_player_position(move_x, move_y, cube))
+	if(!check_player_position(move_x, move_y, cube) && player_dir(cube->player_dx, cube->player_dy))
+	{
+		if (cube->map[(int)(cube->player_dx + cube->player_x)][(int)cube->player_dy] == '#' && cube->map[(int)cube->player_x][(int)(cube->player_dy + cube->player_dy)] == '#')
+			return ;
+		if(cube->map[(int)(cube->player_dx + cube->player_x)][(int)cube->player_dy] == '#' && cube->map[(int)cube->player_x][(int)(cube->player_dy + cube->player_dy)] != '#')
+			cube->player_y = move_y;
+		else if(cube->map[(int)cube->player_x][(int)(cube->player_dy + cube->player_dy)] == '#' && cube->map[(int)(cube->player_dx + cube->player_x)][(int)cube->player_dy] != '#')
+			cube->player_x = move_x;
+	}
+	else if(check_player_position(move_x, move_y, cube))
 	{
 		cube->player_x = move_x;
 		cube->player_y = move_y;
--- render.c
+++ render.c
@@ -39,21 +39,19 @@ int	key_handler(int keycode, t_cube *cube)
 
 int	is_door(t_cube *cube)
 {
-	if(cube->map[(int)(cube->player_x + 0.5)][(int)(cube->player_y)] >= 1000)
+	if(cube->map[(int)(cube->player_x + 0.5)][(int)(cube->player_y)] >= 1001)
 		return (1);
-	if(cube->map[(int)(cube->player_x - 0.5)][(int)(cube->player_y)] >= 1000)
+	if(cube->map[(int)(cube->player_x - 0.5)][(int)(cube->player_y)] >= 1001)
 		return (1);
-	if(cube->map[(int)(cube->player_x)][(int)(cube->player_y + 0.5)] >= 1000)
+	if(cube->map[(int)(cube->player_x)][(int)(cube->player_y + 0.5)] >= 1001)
 		return (1);
-	if(cube->map[(int)(cube->player_x)][(int)(cube->player_y - 0.5)] >= 1000)
+	if(cube->map[(int)(cube->player_x)][(int)(cube->player_y - 0.5)] >= 1001)
 		return (1);
 	return (0);
 }
 
 void	open_door(t_cube *cube)
 {
-	if(is_door(cube))
-		return ;
 	if(cube->map[(int)cube->player_x + 1][(int)cube->player_y] == CLOSE_DOOR)
 		cube->map[(int)cube->player_x + 1][(int)cube->player_y] += DOOR_SPEED;
 	else if(cube->map[(int)cube->player_x - 1][(int)cube->player_y] == CLOSE_DOOR)
